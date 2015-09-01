1d684644ddee72f115a84e904b290dea47d8a591
Phil Christensen <Phil@Phils-MacBook-Pro.local>
2015-08-31 20:06:18 -0500

    Initial Commit

---
+++ Offline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Offline
+//
+//  Created by Phil Christensen on 8/31/15.
+//  Copyright (c) 2015 bigBootyBitches LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
