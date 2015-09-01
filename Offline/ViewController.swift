336fd3f269fcb85482aa20ccf102bcef4097b496
Phil Christensen <Phil@Phils-MacBook-Pro.local>
2015-08-31 22:15:17 -0500

    Test Check in

--- Offline/ViewController.swift
+++ Offline/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let test = 1
-    let test = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
